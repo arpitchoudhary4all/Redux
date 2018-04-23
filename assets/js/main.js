669cb52bbe194b71c82e09a7ae6a09f923b04534
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2018-04-23 20:29:26 +0530

    counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,43 @@
 
 			});
 
+			$.fn.jQuerySimpleCounter = function( options ) {
+				var settings = $.extend({
+					start:  0,
+					end:    100,
+					easing: 'swing',
+					duration: 400,
+					complete: ''
+				}, options );
+		
+				var thisElement = $(this);
+		
+				$({count: settings.start}).animate({count: settings.end}, {
+					duration: settings.duration,
+					easing: settings.easing,
+					step: function() {
+						var mathCount = Math.ceil(this.count);
+						thisElement.text(mathCount);
+					},
+					complete: settings.complete
+				});
+			};
+		
+		
+		$('#number1').jQuerySimpleCounter({end: 12,duration: 3000});
+		$('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
+		$('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
+		$('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
+		
+		
+		
+			  /* AUTHOR LINK */
+			 $('.about-me-img').hover(function(){
+					$('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+				}, function(){
+					$('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+				});
+		  
 		// Footer.
 			$footer.each( function() {
 
