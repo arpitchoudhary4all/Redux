019f4e90383f205ebdc777a093b57cf45bec0070
imansigupta <guptamansi742@gmail.com>
2018-05-05 13:12:03 +0530

    form done

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,54 +1,53 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyABybJq_-PPnIIGNkCHScYb_uU0OukM6Hs",
-    authDomain: "contactform-910e7.firebaseapp.com",
-    databaseURL: "https://contactform-910e7.firebaseio.com",
-    projectId: "contactform-910e7",
-    storageBucket: "contactform-910e7.appspot.com",
-    messagingSenderId: "834090061680"
-  };
-firebase.initializeApp(config);
-
-//Reference message collection
-var messageRef=firebase.database().ref('message');
-
-//Listen for form submit
-document.getElementById('contactform').addEventListener('submit',submitform);
-
-//Submit form execution
-function submitform(e){
-    e.preventDefault();
-    //get values
+        apiKey: "AIzaSyABybJq_-PPnIIGNkCHScYb_uU0OukM6Hs",
+        authDomain: "contactform-910e7.firebaseapp.com",
+        databaseURL: "https://contactform-910e7.firebaseio.com",
+        projectId: "contactform-910e7",
+        storageBucket: "contactform-910e7.appspot.com",
+        messagingSenderId: "834090061680"
+      };
+    firebase.initializeApp(config);
+    
+    //Reference message collection
+    var messageRef=firebase.database().ref('message');
+    
+    //Listen for form submit
+    document.getElementById('contactform').addEventListener('submit',submitform);
+    //Submit form execution
+    function submitform(e){
+        e.preventDefault();
+        //get values
     var name=getInput('name');
-    var email=getInput('email');
-    var phone=getInput('phone');
-    var message=getInput('message');
+       var email=getInput('email');
+        var phone=getInput('phone');
+        var message=getInput('message');
+        saveMessage(name,email,phone,message);
+        //Send mail
+        $.ajax({
+            url: "https://formspree.io/choudharyarpit99@gmail.com", 
+            method: "POST",
+            data: {name,phone,email,message},
+            dataType: "json"
+        });
+        
+        //Save message to database
+        
+    }
     
-    //Send mail
-    var request = new XMLHttpRequest();
-    var form = document.getElementById('contactform');
-    var data = new FormData(form)
-    request.open('POST', 'https://formspree.io/choudharyarpit99@gmail.com', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(data);
+    //Function to get values from form
+    function getInput(id){
+        return document.getElementById(id).value;
+    }
     
-    //Save message to database
-    saveMessage(name,email,phone,message);
-}
-
-//Function to get values from form
-function getInput(id){
-    return document.getElementById(id).value;
-}
-
-//Save message to firebase
-function saveMessage(name,email,phone,message){
-    var newmessageRef=messageRef.push();
-    newmessageRef.set({
-       name:name,
-       email:email,
-       phone:phone,
-       message:message
-    });
-    alert("Thank you for your valuable suggestion")
-}
+    //Save message to firebase
+    function saveMessage(name,email,phone,message){
+        var newmessageRef=messageRef.push();
+        newmessageRef.set({
+           name:name,
+           email:email,
+           phone:phone,
+           message:message
+        });
+        alert("Thanks For Ur valuable Suggestion")
+    } 
